57a772d339010dc5b912197b8235621d4a45a575
Volker Scheuber <volker.scheuber@forgerock.com>
2020-01-27 11:18:00 -0600

    Improved IDM init_config.sh script to detect if a run-time configuration already exists.

--- idm/run/init_config.sh
+++ idm/run/init_config.sh
@@ -2,5 +2,15 @@
 cd "$(dirname "$0")"
 
 echo Initializing IDM configuration...
+if [ -d config/idm/conf ] ; then
+    read -p "Existing IDM run-time configuration found. Do you want to continue and reset the IDM configuration? (N/y): " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]] ; then
+        echo "Resetting IDM configuration..."
+        rm -rf config/idm/*
+    else
+        1>&2 echo "Aborted."
+        exit 0
+    fi
+fi
 cp -av ../build/config/idm/ config/idm
 echo Done.
